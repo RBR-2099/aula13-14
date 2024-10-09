70b28cd1b257df32aeeefb9628f96239931c86c5
Rodrigo Reis <rodrigo.br.reis@gmail.com>
2024-10-09 20:09:04 -0300

    salvando a base da branch e os primeiros exercícios

---
+++ exercicio1.js
@@ -0,0 +1,13 @@
+const filme = {
+    nome: "O Iluminado", 
+    anoLançamento: 1980,
+    elenco: ["Jack Nicholson" , "Shelley Duvall" , "Danny Lloyd "],
+    viuNaoViu: "true",
+}
+
+console.log (filme.nome)
+console.log (filme.anoLançamento)
+console.log ([filme.elenco])
+console.log ([filme.viuNaoViu])
+
+  
